24cdd8c5fbb2a168d192055430e4b95824582795
Jose Rodriguez <joseluis1409rodriguez@gmail.com>
2026-03-31 20:39:06 -0500

    agrego script para Railway

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd api
+
+composer install --no-dev --optimize-autoloader
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+php artisan serve --host=0.0.0.0 --port=$PORT
