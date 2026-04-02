362d568bb9fb185a7e0fff289173d913fedb3f50
Jose Rodriguez <joseluis1409rodriguez@gmail.com>
2026-04-01 20:37:42 -0500

    Actualizo configuracion de railway

--- api/config/database.php
+++ api/config/database.php
@@ -87,7 +87,7 @@ return [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5433'),
+            'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
