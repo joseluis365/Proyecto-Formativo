62203d59be0fe8ef68fa027d1bbb5b223a01e258
Jose Rodriguez <joseluis1409rodriguez@gmail.com>
2026-04-01 20:45:58 -0500

    Actualizo configuracion de railway

--- api/config/database.php
+++ api/config/database.php
@@ -90,7 +90,7 @@ return [
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'railway'),
             'username' => env('DB_USERNAME', 'postgres'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', 'XncHTQXgWnOpgaTfCDVqSdOQQmmRlSJb'),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
