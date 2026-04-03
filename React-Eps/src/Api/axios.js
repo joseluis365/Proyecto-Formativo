ccee80bca4e2d41a6590bc31223901b40b54c4f2
Jose Rodriguez <joseluis1409rodriguez@gmail.com>
2026-04-02 21:54:37 -0500

    configurar axios para vercel

--- React-Eps/src/Api/axios.js
+++ React-Eps/src/Api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Swal from "sweetalert2";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: `${import.meta.env.VITE_API_BASE_URL || "http://localhost:8000"}/api`,
   headers: {
     "Content-Type": "application/json",
   },
