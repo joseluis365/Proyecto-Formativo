6dfb9d172af10042b2e7b2d6668e2d78ee33f46c
Jose Rodriguez <joseluis1409rodriguez@gmail.com>
2026-04-02 22:11:29 -0500

    configuracion railway

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -21,5 +21,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Carbon::setLocale('es');
+
+        if (env('APP_ENV') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
